ea2a49d9c0721bb889592cd20d2884609b4ffeb6
Jenwit Penjamrat <jenwit_penjamrat@hotmail.com>
2019-01-29 11:47:00 +0700

    Initial Project

---
+++ src/main/java/com/bank/springjunit/SpringjunitApplication.java
@@ -0,0 +1,14 @@
+package com.bank.springjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjunitApplication.class, args);
+	}
+
+}
+
